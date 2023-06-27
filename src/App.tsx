62129c493cf9a3c96601ccceb9190f47cc334b09
kmvpvl <43674119+kmvpvl@users.noreply.github.com>
2023-06-27 08:44:10 +0300

    proto with login form

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import User from './components/user';
+
+interface IAppProps {
+
+}
+interface IAppState {
+
+}
+export default class App extends React.Component<IAppProps, IAppState> {
+    render(): React.ReactNode {
+        return (<>
+        <div className='home-container'>
+            <img src="./favicon-32x32.png" alt=''/>
+        </div>
+        <div className='location-selector-container'>Location</div>
+        <User/>
+        <div className='layers-selector-container'>Layers</div>
+        <div className='devices-management-container'>Devices</div>
+        </>
+        );
+    }
+}
